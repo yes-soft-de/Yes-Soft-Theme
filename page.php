5bf7e1be23070aa4c37569151f24588525283cd0
Talal Danoon <tald7344@gmail.com>
2019-06-20 18:42:42 +0300

    Modify Services Page

--- page.php
+++ page.php
@@ -1,44 +1,116 @@
 <?php get_header() ?>
-
+    <!--This Page Is For Services Page-->
     <!--STart Our Posts Page-->
-    <div class="our-posts my-4">
+    <div class="our-posts our-services my-4">
         <div class="container">
             <div class="row">
-                <div class="col-md-8 mx-auto">
-                    <div class="post p-3">
-                        <h2 class="h1 post-title text-center">Our Services</h2>
-                        <div class="post-content text-center my-3 pb-4">
-                            <p class="text-left">
+                <div class="col-md-12">
+                    <div class="col-md-11 mx-auto post pt-3">
+                        <h2 class="h1 post-title text-center p-0">Our Services</h2>
+                        <div class="post-content mb-3 pb-2">
+                            <p class="text-center m-0">
                                 Our highly qualified and passionate team provide a full range of software development
                                 services to individuals as well as big companies including:
-                                <br><br>
-                                <img src="http://via.placeholder.com/400x400" class="curve" alt="">
-                                <p>
-                                <strong class="h4 text-secondary">UI/ UX Solutions:</strong>
-                                UX Design refers to the term User Experience Design, while UI Design stands for User
-                                Interface Design. Both elements are crucial to a product and work closely together. But
-                                despite their professional relationship, the roles themselves are quite different,
-                                referring to very different parts of the process and the design discipline. Where UX
-                                Design is a more analytical and technical field, UI Design is closer to what we refer to
-                                as graphic design, though the responsibilities are somewhat more complex.<br><br>
-
                             </p>
-                                <img src="http://via.placeholder.com/400x400" class="curve" alt="">
-                                <p>
-                                    <strong class="h4 text-secondary">Web Development:</strong>
-                                    Web development is the work involved in developing a web site for the Internet or an
-                                    intranet. Web development can range from developing a simple single static page of plain
-                                    text to complex web-based internet applications, electronic businesses, and social
-                                    network services.
-                                    Mobile Development: Mobile app development is the act or process by which a mobile app
-                                    is developed for mobile devices, such as personal digital assistants, enterprise digital
-                                    assistants or mobile phones.
-                                </p>
-                            </p>
-
                         </div>
                     </div>
-                </div>
+                    <!--Start Ux Ui Solution Section-->
+                    <div class="post p-3">
+                        <div class="post-content text-center">
+                            <div class="row">
+                                <div class="col-md-3 image">
+                                    <img src="<?php echo get_template_directory_uri() . '/img/UX_UI_Selution.jpeg'; ?>" class="img-fluid img-thumbnail" alt="Ux Ui Selution">
+                                </div>
+                                <div class="col-md-9 content">
+                                    <p>
+                                    <strong class="h4 text-secondary">UI/ UX Solutions:</strong>
+                                    UX Design refers to the term User Experience Design, while UI Design stands for User Interface Design. UX is all about concentrating on the user. A successful user experience is one that impresses the user by making his interaction smooth and useful, eventually leading to better customer satisfaction. With design thinking at the centre of all our solutions, our expertise lies in delivering great user experiences across different visual platforms.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                   <!--Start Web Developments Section-->
+                    <div class="post p-3">
+                        <div class="post-content text-center">
+                            <div class="row">
+                                <div class="col-md-9 content">
+                                    <p>
+                                        <strong class="h4 text-secondary">Web Development:</strong>
+                                        It’s one of the most growing fields in IT domain. From simple static to complex web-based internet apps, our qualified team is ready. We create new sites according to your needs and maintain your current website, too. 
+                                    </p>
+                                </div>
+                                <div class="col-md-3 image">
+                                    <img src="<?php echo get_template_directory_uri() . '/img/web-development.jpg'; ?>" class="img-fluid img-thumbnail" alt="Web Development">
+                                </div>                                
+                            </div>
+                        </div>
+                    </div>
+                   <!--Start Wordpress Solutions Section-->
+                    <div class="post p-3">
+                        <div class="post-content text-center">
+                            <div class="row">
+                                <div class="col-md-3 image">
+                                    <img src="<?php echo get_template_directory_uri() . '/img/wordpress-solution.png'; ?>" class="img-fluid img-thumbnail" alt="Wordpress Solution">
+                                </div>
+                                <div class="col-md-9 content">
+                                    <p>
+                                    <strong class="h4 text-secondary">Wordpress Solutions:</strong>
+                                    Content management systems like WordPress is used more than standard static HTML sites now. This platform gives the ability to create a site that meets your needs. We have developed sites for this platform and can do everything from designing a site from scratch, developing custom plug-ins, to setting up custom features and functionalities
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                   <!--Start App Developments Section-->
+                    <div class="post p-3">
+                        <div class="post-content text-center">
+                            <div class="row">
+                                <div class="col-md-9 content">
+                                    <p>
+                                        <strong class="h4 text-secondary">App Development:</strong>
+                                        Nowadays, many people use mobile phones to access the internet which means an important audience we can take care of for your company. Our app development team is ready to create apps for any platform you want (Android and iOS).
+                                    </p>
+                                </div>
+                                <div class="col-md-3 image">
+                                    <img src="<?php echo get_template_directory_uri() . '/img/app-development.jpg'; ?>" class="img-fluid img-thumbnail" alt="App Development">
+                                </div>                                
+                            </div>
+                        </div>
+                    </div>                  
+                   <!--Start E-Marketing Section-->
+                   <div class="post p-3">
+                        <div class="post-content text-center">
+                            <div class="row">
+                                <div class="col-md-3 image">
+                                    <img src="<?php echo get_template_directory_uri() . '/img/e-marketing.png'; ?>" class="img-fluid img-thumbnail" alt="E-Marketing">
+                                </div>                              
+                                <div class="col-md-9 content">
+                                    <p>
+                                        <strong class="h4 text-secondary">E-Marketing:</strong>
+                                        it’s the mix of modern communication technology and traditional principles that marketers usually apply. E-marketing is important for your business because when it is executed properly, the return on investment can be greater than more traditional marketing strategies. Our specialist marketers offer the complete marketing service to your business.
+                                    </p>
+                                </div>                              
+                            </div>
+                        </div>
+                    </div>                       
+                   <!--Start Graphic design Section-->
+                   <div class="post p-3">
+                        <div class="post-content text-center">
+                            <div class="row">
+                                <div class="col-md-9 content">
+                                    <p>
+                                        <strong class="h4 text-secondary">Graphic design:</strong>
+                                        It’s the process of visual communication and problem-solving through the use of typography, photography, and illustration. The field is considered a subset of visual communication and communication design. Our unique and creative services will help in showcasing the uniqueness of your business to your prospective and present customers.
+                                    </p>
+                                </div>
+                                <div class="col-md-3 image">
+                                    <img src="<?php echo get_template_directory_uri() . '/img/graphic-design.jpg'; ?>" class="img-fluid img-thumbnail" alt="Graphic design">
+                                </div>                                
+                            </div>
+                        </div>
+                    </div>                       
+                </div>               
             </div>
         </div>
     </div>
