f3901c06f08d9c09854404a1bfa529b862d7a382
Talal Danoun <tald7344@gmail.com>
2020-01-08 14:31:31 +0200

    finish about us separate page

--- about-us.php
+++ about-us.php
@@ -3,114 +3,116 @@
 <?php get_header(); ?>
 
 <!--Start About Us Separate Page-->
-<div class="about-us-separate-page">
-<!--	<div class="container">-->
-		<div class="about-us-section">
-			<div class="container-fluid">
-				<div class="row">
-                    <div class="col-6 d-sm-none mx-auto pl-0">
-                        <img src="<?php echo get_template_directory_uri() . '/img/laptop.png'; ?>" class="img-fluid" alt="laptop Image">
+<div class="about-us-separate-page position-relative">
+    <img src="<?php echo get_template_directory_uri() . '/img/about-us-separate-background.png'; ?>" class="d-block d-sm-none d-md-block responsive-element position-absolute" >
+    <img src="<?php echo get_template_directory_uri() . '/img/mobile-about-us-separate-background.png'; ?>" class="d-none d-sm-block d-md-none responsive-element position-absolute" >
+    <div class="">
+    <div class="about-us-section">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-6 d-sm-none mx-auto pl-0">
+                    <img src="<?php echo get_template_directory_uri() . '/img/laptop.png'; ?>" class="img-fluid" alt="laptop Image">
+                </div>
+                <div class="col-12 col-sm-7 offset-md-1">
+                    <hr class="mb-0 ml-2 mt-1">
+                    <p class="text-justify">
+                        <strong>Yes Soft</strong> is an ecommerce specialized software company located in Berlin/ Germany. It adopts open-source methodology. All its projects’ codes are available on <a href="https://github.com/yes-soft-de/">Github</a>.<br>
+                        Its team consists of developers, designers, content makers, testers and team managers. They work together remotely and results are always great.
+                        They use up-to-date technologies to bring your imaginary requirements to reality.<br>
+                        Are you still hesitated about the company? Why don't you check out <a href="http://ishtar-art.de/">ISHTAR ART</a> to evaluate everything by yourself?
+                    </p>
+                </div>
+                <div class="col-12 col-sm-5 col-md-4 col-lg-3 col-xl-2 d-none d-sm-block pl-0">
+                    <img src="<?php echo get_template_directory_uri() . '/img/laptop.png'; ?>" class="img-fluid" alt="laptop Image">
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="about-e-commerce-section py-5">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-6 col-md-4 col-xl-3 mx-auto mx-md-0">
+                    <div class="image-section mx-auto">
+                        <img src="<?php echo get_template_directory_uri() . '/img/quality-e-commerce.jpg'; ?>" class="img-fluid" alt="laptop Image">
+                        <p class="image-title-section text-center font-weight-bold">Ecommerce</p>
                     </div>
-					<div class="col-12 col-sm-7 offset-md-1">
-                        <hr class="mb-0 ml-2 mt-1">
-						<p class="text-justify">
-							<strong>Yes Soft</strong> is an ecommerce specialized software company located in Berlin/ Germany. It adopts open-source methodology. All its projects’ codes are available on <a href="https://github.com/yes-soft-de/">Github</a>.<br>
-							Its team consists of developers, designers, content makers, testers and team managers. They work together remotely and results are always great.
-							They use up-to-date technologies to bring your imaginary requirements to reality.<br>
-							Are you still hesitated about the company? Why don't you check out <a href="http://ishtar-art.de/">ISHTAR ART</a> to evaluate everything by yourself?
-						</p>
-					</div>
-					<div class="col-12 col-sm-5 col-md-4 col-lg-3 col-xl-2 d-none d-sm-block pl-0">
-                        <img src="<?php echo get_template_directory_uri() . '/img/laptop.png'; ?>" class="img-fluid" alt="laptop Image">
-					</div>
-				</div>
-			</div>
-		</div>
-		<div class="about-e-commerce-section py-5">
-			<div class="container-fluid">
-				<div class="row">
-					<div class="col-6 col-md-4 col-xl-3 mx-auto mx-md-0">
-						<div class="image-section mx-auto">
-							<img src="<?php echo get_template_directory_uri() . '/img/quality-e-commerce.jpg'; ?>" class="img-fluid" alt="laptop Image">
-                            <p class="image-title-section text-center font-weight-bold">Ecommerce</p>
-                        </div>
-					</div>
-					<div class="col-12 col-sm-10 col-md-8 col-lg-7 pt-5 mx-auto mx-md-0">
-						<p class="text-justify">
-							<strong>E-Commerce</strong> means buying and selling of goods, products, or services over the internet. These services provided online over the internet network. Transaction of money, funds, and data are also considered as E-commerce.
-							At Yes Soft we adopted Ecommerce as a main working discipline. It’s the future as we see it. We have an online-working team. So, we concentrates on it as it has similarities with our vision. We are looking forward and working hard to be the first company that deliver this service in the whole world. We know that there are lots of companies working in this discipline out there and the competition is hot, but we know ourselves very good, too.
-							<br /><br/>
-							Are you excited to read more?
-							Here we go.<br/>
-							At <strong>Yes Soft</strong>, we introduce a variety of ways to build Ecommerce websites and apps. From using customizable platforms like wordpress and <a href="https://www.prestashop.com/en">Prestashop</a> to building a brand new website from zero like we did at our project <a href="http://ishtar-art.de/">ISHTAR</a>.
-							<span>
-								Is this all what we introduce?<br/>
-								No, of course. We also develop Android apps for the same target, professional Ecommerce platforms.
-							</span>
-							<span>
-								Do our work about Ecommerce only?<br/>
-								No, we also develop websites and apps for any work discipline. But, our expertise is Ecommerce related projects.
-							</span>
-						</p>
-					</div>
-				</div>
-			</div>
-		</div>
-		<div class="about-marketing-section py-5">
-			<div class="container-fluid">
-				<div class="row">
-                    <div class="col-6 col-md-4 col-xl-3 mx-auto mx-md-0 d-md-none">
-                        <div class="image-section mx-auto">
-                            <img src="<?php echo get_template_directory_uri() . '/img/front-end.jpg'; ?>" class="img-fluid" alt="laptop Image">
-                            <p class="image-title-section text-center font-weight-bold">Ecommerce</p>
-                        </div>
+                </div>
+                <div class="col-12 col-sm-10 col-md-8 col-lg-7 pt-5 mx-auto mx-md-0">
+                    <p class="text-justify">
+                        <strong>E-Commerce</strong> means buying and selling of goods, products, or services over the internet. These services provided online over the internet network. Transaction of money, funds, and data are also considered as E-commerce.
+                        At Yes Soft we adopted Ecommerce as a main working discipline. It’s the future as we see it. We have an online-working team. So, we concentrates on it as it has similarities with our vision. We are looking forward and working hard to be the first company that deliver this service in the whole world. We know that there are lots of companies working in this discipline out there and the competition is hot, but we know ourselves very good, too.
+                        <br /><br/>
+                        Are you excited to read more?
+                        Here we go.<br/>
+                        At <strong>Yes Soft</strong>, we introduce a variety of ways to build Ecommerce websites and apps. From using customizable platforms like wordpress and <a href="https://www.prestashop.com/en">Prestashop</a> to building a brand new website from zero like we did at our project <a href="http://ishtar-art.de/">ISHTAR</a>.
+                        <span>
+                            Is this all what we introduce?<br/>
+                            No, of course. We also develop Android apps for the same target, professional Ecommerce platforms.
+                        </span>
+                        <span>
+                            Do our work about Ecommerce only?<br/>
+                            No, we also develop websites and apps for any work discipline. But, our expertise is Ecommerce related projects.
+                        </span>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="about-marketing-section py-5">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-6 col-md-4 col-xl-3 mx-auto mx-md-0 d-md-none">
+                    <div class="image-section mx-auto">
+                        <img src="<?php echo get_template_directory_uri() . '/img/shopping-basket.png'; ?>" class="img-fluid" alt="laptop Image">
+                        <p class="image-title-section text-center font-weight-bold">Ecommerce</p>
                     </div>
-					<div class="col-12 col-sm-10 col-md-7 col-xl-8 offset-sm-1 pt-5">
-						<p class="text-justify">
-							<strong>E-marketing</strong> is the process of marketing a product or service using the Internet. E-marketing not only includes marketing on the Internet, but also includes marketing done via email and wireless media. It uses a range of technologies to help connect businesses to their customers.
-							<br/><br/>
-							As <strong>Yes Soft</strong> has an online-working team, it has many things in common with all electronic activities such as Ecommerce we talked about before. <strong>E-marketing</strong> lays in the same perspective for us. It’s an active discipline in today’s world. And it completes our working strategy, marketing projects we developed.
-							<br/>
-							To reach this point, we deploy several aspects by our high-professional team. For example, we organize social-media and email promotional campaigns as well as content marketing. We make Flyer design which is the first impression a potential client may take. We design business cards, too.
-							You may ask yourself, Does these activities have potential?
-							Of course they have. Do you know that E-marketing reduces marketing campaign cost as the marketing is done through the internet. And it facilitates monitoring through the web tracking capabilities. Finally, do you realize before how many hours do you spend online daily? Answer this question and you’ll get the point yourself.
-						</p>
-					</div>
-					<div class="col-md-4 col-xl-3 mx-auto mx-md-0 d-none d-md-block" style="overflow: hidden;">
-						<div class="image-section mx-auto">
-							<img src="<?php echo get_template_directory_uri() . '/img/front-end.jpg'; ?>" class="img-fluid" alt="laptop Image">
-                            <p class="image-title-section text-center font-weight-bold">Marketing</p>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-		<div class="about-remote-section pt-5">
-			<div class="container-fluid">
-				<div class="row">
-					<div class="col-6 col-md-4 col-xl-3 mx-auto mx-md-0">
-						<div class="image-section mx-auto">
-							<img src="<?php echo get_template_directory_uri() . '/img/front-end.jpg'; ?>" class="img-fluid" alt="laptop Image">
-                            <p class="image-title-section text-center font-weight-bold">Remote Maintenance</p>
-                        </div>
-					</div>
-					<div class="col-12 col-sm-10 col-md-8 col-lg-7 pt-5 mx-auto mx-md-0">
-						<p class="text-justify">
-							The third important side to tell you about in order to complete Our base triangle is: <strong>Remote maintenance</strong>.
-							<br/><br/>
-							<strong>Remote maintenance</strong> is a service we offer to companies and individuals in order to tell them what's wrong with their websites, suggest solutions and apply them if they order.
-							<br/><br/>
-							We take care of plenty of problems related to Browser compatibility problems, Responsive issues, Not working links and forms, Not removed indexing codes, Slow sites problem and more.
-							<br/><br/>
-							Our fully-remotely working team can handle all that and more. We can hire one person or our full team capacity to help you.
-							<br/><br/>
-							After all what you read about us, are you still hesitated to try our services? We promise, you won't regret.
-						</p>
-					</div>
-				</div>
-			</div>
-		</div>
-<!--	</div>-->
+                </div>
+                <div class="col-12 col-sm-10 col-md-7 col-xl-8 offset-sm-1 pt-5">
+                    <p class="text-justify">
+                        <strong>E-marketing</strong> is the process of marketing a product or service using the Internet. E-marketing not only includes marketing on the Internet, but also includes marketing done via email and wireless media. It uses a range of technologies to help connect businesses to their customers.
+                        <br/><br/>
+                        As <strong>Yes Soft</strong> has an online-working team, it has many things in common with all electronic activities such as Ecommerce we talked about before. <strong>E-marketing</strong> lays in the same perspective for us. It’s an active discipline in today’s world. And it completes our working strategy, marketing projects we developed.
+                        <br/>
+                        To reach this point, we deploy several aspects by our high-professional team. For example, we organize social-media and email promotional campaigns as well as content marketing. We make Flyer design which is the first impression a potential client may take. We design business cards, too.
+                        You may ask yourself, Does these activities have potential?
+                        Of course they have. Do you know that E-marketing reduces marketing campaign cost as the marketing is done through the internet. And it facilitates monitoring through the web tracking capabilities. Finally, do you realize before how many hours do you spend online daily? Answer this question and you’ll get the point yourself.
+                    </p>
+                </div>
+                <div class="col-md-4 col-xl-3 mx-auto mx-md-0 d-none d-md-block" style="overflow: hidden;">
+                    <div class="image-section mx-auto">
+                        <img src="<?php echo get_template_directory_uri() . '/img/shopping-basket.png'; ?>" class="img-fluid" alt="laptop Image">
+                        <p class="image-title-section text-center font-weight-bold">Marketing</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="about-remote-section pt-5">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-6 col-md-4 col-xl-3 mx-auto mx-md-0">
+                    <div class="image-section mx-auto">
+                        <img src="<?php echo get_template_directory_uri() . '/img/website-test.png'; ?>" class="img-fluid" alt="laptop Image">
+                        <p class="image-title-section text-center font-weight-bold">Remote Maintenance</p>
+                    </div>
+                </div>
+                <div class="col-12 col-sm-10 col-md-8 col-lg-7 pt-5 mx-auto mx-md-0">
+                    <p class="text-justify">
+                        The third important side to tell you about in order to complete Our base triangle is: <strong>Remote maintenance</strong>.
+                        <br/><br/>
+                        <strong>Remote maintenance</strong> is a service we offer to companies and individuals in order to tell them what's wrong with their websites, suggest solutions and apply them if they order.
+                        <br/><br/>
+                        We take care of plenty of problems related to Browser compatibility problems, Responsive issues, Not working links and forms, Not removed indexing codes, Slow sites problem and more.
+                        <br/><br/>
+                        Our fully-remotely working team can handle all that and more. We can hire one person or our full team capacity to help you.
+                        <br/><br/>
+                        After all what you read about us, are you still hesitated to try our services? We promise, you won't regret.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
 </div> <!--.about-us-separate-page-->
 <!--End About Us Separate Page-->
 
