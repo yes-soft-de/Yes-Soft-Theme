ea6876fb67947801161259d10348946c0f1974a9
Talal Danoun <tald7344@gmail.com>
2020-01-13 18:14:22 +0200

    remove the test code from author page

--- author-old-file.php
+++ author-old-file.php
@@ -2,7 +2,10 @@
 $curauth = $wp_query->get_queried_object();
 $authid = $curauth->ID;
 ?>
-	Email: <?php echo $curauth->user_email; ?>
-	Website: <?php echo $curauth->user_url; ?>
-	Name: <?php echo $curauth->user_firstname . " " . $curauth->user_lastname; ?>
-	Bio: <?php echo $curauth->user_description; ?>
+    Email: <?php echo $curauth->user_email; ?>
+    Website: <?php echo $curauth->user_url; ?>
+    job: <?php echo $curauth->job_title; ?>
+    company: <?php echo $curauth->company_name; ?>
+    Name: <?php echo $curauth->user_firstname . " " . $curauth->user_lastname; ?>
+    Bio: <?php echo $curauth->user_description; ?>
+
